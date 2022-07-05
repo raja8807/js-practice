f79cf07b0c80a69d1353ce89c604dd36dc845af8
raja <s.rajarathinam1999@gmail.com>
2022-07-05 14:24:24 +0530

    task-1

---
+++ arithmetics/main.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 5;
+
+let add = a + b;
+let sub = a - b;
+let div = a / b;
+let multiply = a * b;
+
+console.log("Number 1 is " + a)
+console.log("Number 2 is " + b)
+
+console.log("The Addition is " + add);
+console.log("The Subtaction is " + sub);
+console.log("The Division is " + div);
+console.log("The Multiplication is " + multiply);
