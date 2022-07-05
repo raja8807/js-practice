59673c5e04ecf55576b4dc9ed6875a5b204e76a8
raja <s.rajarathinam1999@gmail.com>
2022-07-05 14:54:00 +0530

    task-4

---
+++ task-4/main.js
@@ -0,0 +1,10 @@
+let p = 1000;
+let r = 5;
+let t = 5;
+
+let simpleInterest = (p*r*t)/100;
+
+console.log("Principal amuount is "+p)
+console.log("Rate of interest is "+r+ "%")
+console.log("Period is "+t+" years")
+console.log("Simple interest is "+simpleInterest)
