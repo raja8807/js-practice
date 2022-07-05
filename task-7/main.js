e865554b9ea7f37ca87646d09be733170cbfda00
raja <s.rajarathinam1999@gmail.com>
2022-07-05 15:24:58 +0530

    task-7

---
+++ task-7/main.js
@@ -0,0 +1,14 @@
+let num1 = 7;
+let num2 = 2;
+let num3 = 9;
+let num4 = 6;
+
+let n = 4;
+
+let total = num1 + num2 + num3 + num4;
+
+console.log("The total is "+total);
+
+let avg = total/n;
+
+console.log("The avrage is " + avg);
